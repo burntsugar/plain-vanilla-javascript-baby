3a98c04e28015375df5751be9cd701f890b5bce4
burntsugar <rrakky89@gmail.com>
2020-01-05 16:32:34 +1100

    Implemented cache timing check

--- scripts/github-deets.js
+++ scripts/github-deets.js
@@ -11,39 +11,39 @@ import { Rezponse } from './rezponse.js'
 
 
 const appVars = {
-    PARENT_WRAPPER_ID: 'github-user-wrapper',
-    MESSAGE_USER_NOT_FOUND: 'not found!',
+    MESSAGE_USER_NOT_FOUND: 'Not found!',
     MESSAGE_OPERATION_CANT_COMPLETE: 'Operation could not be completed at this time!',
     MESSAGE_USERNAME_CANNOT_BE_EMPTY: 'Github username cannot be empty!',
-    ERROR_FAILED: 'Operation failed!',
-    ERROR_NO_NETWORK: 'No network!',
+    MESSAGE_ERROR_FAILED: 'Operation failed!',
+    MESSAGE_ERROR_NO_NETWORK: 'No network!',
     URL_GITHUB_USER_API: 'https://api.github.com/users/',
-    NODE_ERROR_NODE_ID: 'error-node',
-    HEADING_USERNAME_ID: 'h1-username',
-    IMAGE_USER_ID: 'img-user',
-    UL_USER_DEETS_ID: 'ul-user-deets',
-    DIV_INPUT_USERNAME_CONTROLS_ID: 'input-username-controls',
-    DIV_RESET_USERNAME_CONTROLS_ID: 'reset-username-controls',
+    ID_PARENT_WRAPPER: 'github-user-wrapper',
+    ID_NODE_ERROR_NODE: 'error-node',
+    ID_HEADING_USERNAME: 'h1-username',
+    ID_IMAGE_USER: 'img-user',
+    ID_UL_USER_DEETS: 'ul-user-deets',
+    ID_DIV_INPUT_USERNAME_CONTROLS: 'input-username-controls',
+    ID_DIV_RESET_USERNAME_CONTROLS: 'reset-username-controls',
     LOCAL_STORAGE_CACHE_TIME: 60000,
-    LOCAL_STORAGE_OBJECT_FOUND_STATUS: 2000,
-    LOCAL_STORAGE_OBJECT_NOT_FOUND_STATUS: -1000,
-    LOCAL_STORAGE_OBJECT_EXPIRED_STATUS: -2000,
-    NETWORK_NOT_AVAILABLE_STATUS: -3000,
-    USERNAME_IS_EMPTY_STATUS: -4000,
-    START_NEW_REQUEST: 7000,
-    GITHUB_USER_NOT_FOUND_STATUS: -5000,
-    GITHUB_USER_FOUND_STATUS: 8000,
-    CANT_COMPLETE_OPERATION_STATUS: -7000
+    STATUS_LOCAL_STORAGE_OBJECT_FOUND: 2000,
+    STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND: -1000,
+    STATUS_LOCAL_STORAGE_OBJECT_EXPIRED: -2000,
+    STATUS_NETWORK_NOT_AVAILABLE: -3000,
+    STATUS_USERNAME_IS_EMPTY: -4000,
+    STATUS_START_NEW_REQUEST: 7000,
+    STATUS_GITHUB_USER_NOT_FOUND: -5000,
+    STATUS_GITHUB_USER_FOUND: 8000,
+    STATUS_CANT_COMPLETE_OPERATION: -7000
 }
 
 export function getGithubProfile(username) {
     var uname = cleanString(username);
-    if (usernameIsEmpty(uname)) respond(new Rezponse(appVars.USERNAME_IS_EMPTY_STATUS, null))
+    if (usernameIsEmpty(uname)) respond(new Rezponse(appVars.STATUS_USERNAME_IS_EMPTY, null))
     var cachedRezponse = cachedInLocalStorage(uname);
-    if (cachedRezponse.status == appVars.LOCAL_STORAGE_OBJECT_FOUND_STATUS) { 
-        respond(new Rezponse(appVars.LOCAL_STORAGE_OBJECT_FOUND_STATUS, cachedRezponse.body))
+    if (cachedRezponse.status == appVars.STATUS_LOCAL_STORAGE_OBJECT_FOUND) { 
+        respond(new Rezponse(appVars.STATUS_LOCAL_STORAGE_OBJECT_FOUND, cachedRezponse.body))
     } else {
-        respond(new Rezponse(appVars.START_NEW_REQUEST, {login: uname}))
+        respond(new Rezponse(appVars.STATUS_START_NEW_REQUEST, {login: uname}))
     }
 }
 
@@ -57,25 +57,25 @@ function usernameIsEmpty(uname) {
 
 function respond(rezponseObj) {
     switch(rezponseObj.status) {
-        case appVars.USERNAME_IS_EMPTY_STATUS:
+        case appVars.STATUS_USERNAME_IS_EMPTY:
             prepareErrorNode(appVars.MESSAGE_USERNAME_CANNOT_BE_EMPTY)
         break
-        case appVars.LOCAL_STORAGE_OBJECT_FOUND_STATUS:
+        case appVars.STATUS_LOCAL_STORAGE_OBJECT_FOUND:
             prepareSuccessNodes(rezponseObj)
         break
-        case appVars.START_NEW_REQUEST:
+        case appVars.STATUS_START_NEW_REQUEST:
             startNetworkRequest(rezponseObj.body.login)
         break
-        case appVars.GITHUB_USER_FOUND_STATUS:
+        case appVars.STATUS_GITHUB_USER_FOUND:
             prepareSuccessNodes(rezponseObj)
         break
-        case appVars.GITHUB_USER_NOT_FOUND_STATUS:
+        case appVars.STATUS_GITHUB_USER_NOT_FOUND:
             prepareErrorNode(appVars.MESSAGE_USER_NOT_FOUND)
         break
-        case appVars.NETWORK_NOT_AVAILABLE_STATUS:
-            prepareErrorNode(appVars.ERROR_NO_NETWORK)
+        case appVars.STATUS_NETWORK_NOT_AVAILABLE:
+            prepareErrorNode(appVars.MESSAGE_ERROR_NO_NETWORK)
         break
-        case appVars.CANT_COMPLETE_OPERATION_STATUS:
+        case appVars.STATUS_CANT_COMPLETE_OPERATION:
             prepareErrorNode(appVars.MESSAGE_OPERATION_CANT_COMPLETE)
         break
     }
@@ -83,18 +83,18 @@ function respond(rezponseObj) {
 
 function startNetworkRequest(uname) {
     if (!hasConnection()) {
-        processNetworkResult(new Rezponse(appVars.NETWORK_NOT_AVAILABLE_STATUS, null), uname);
+        processNetworkResult(new Rezponse(appVars.STATUS_NETWORK_NOT_AVAILABLE, null), uname);
     } else {
         var queryURL = appVars.URL_GITHUB_USER_API + uname;
         fetchJsonResource(queryURL).then(function (response) {
             var obj = response
            return processNetworkResult(obj, uname)
         }, function (error) {
-            console.error(appVars.ERROR_FAILED, error);
+            console.error(appVars.MESSAGE_ERROR_FAILED, error);
             if (error.name == 'NetworkError') {
-                return processNetworkResult(new Rezponse(appVars.NETWORK_NOT_AVAILABLE_STATUS), null)
+                return processNetworkResult(new Rezponse(appVars.STATUS_NETWORK_NOT_AVAILABLE), null)
             } else {
-                return processNetworkResult(new Rezponse(appVars.CANT_COMPLETE_OPERATION_STATUS), null)
+                return processNetworkResult(new Rezponse(appVars.STATUS_CANT_COMPLETE_OPERATION), null)
             }
         })
     }
@@ -102,37 +102,37 @@ function startNetworkRequest(uname) {
 
 function processNetworkResult(rezponseObj, uname) {
     if (rezponseObj.status == 200) {
-        respond(new Rezponse(appVars.GITHUB_USER_FOUND_STATUS, rezponseObj.body))
+        respond(new Rezponse(appVars.STATUS_GITHUB_USER_FOUND, rezponseObj.body))
     } else if (rezponseObj.status == 404) {
-        respond(new Rezponse(appVars.GITHUB_USER_NOT_FOUND_STATUS, null))
-    } else if (rezponseObj.status == appVars.NETWORK_NOT_AVAILABLE_STATUS) {
-        respond(new Rezponse(appVars.NETWORK_NOT_AVAILABLE_STATUS, null))
+        respond(new Rezponse(appVars.STATUS_GITHUB_USER_NOT_FOUND, null))
+    } else if (rezponseObj.status == appVars.STATUS_NETWORK_NOT_AVAILABLE) {
+        respond(new Rezponse(appVars.STATUS_NETWORK_NOT_AVAILABLE, null))
     } else {
-        respond(new Rezponse(appVars.CANT_COMPLETE_OPERATION_STATUS, null))
+        respond(new Rezponse(appVars.STATUS_CANT_COMPLETE_OPERATION, null))
     }
 }
 
 function prepareErrorNode(errorMessage) {
-    removeNode(appVars.PARENT_WRAPPER_ID, appVars.NODE_ERROR_NODE_ID)
-    pText(appVars.PARENT_WRAPPER_ID, { 'id': appVars.NODE_ERROR_NODE_ID }, errorMessage)
+    removeNode(appVars.ID_PARENT_WRAPPER, appVars.ID_NODE_ERROR_NODE)
+    pText(appVars.ID_PARENT_WRAPPER, { 'id': appVars.ID_NODE_ERROR_NODE }, errorMessage)
 }
 
 function prepareSuccessNodes(rezponseObj) {
     toggleControlVisibility()
-    removeNode(appVars.PARENT_WRAPPER_ID, appVars.NODE_ERROR_NODE_ID)
-    h1Heading(appVars.PARENT_WRAPPER_ID, { 'id': appVars.HEADING_USERNAME_ID }, rezponseObj.body.login)
-    ulList(rezponseObj.body, { 'id': appVars.UL_USER_DEETS_ID }, appVars.PARENT_WRAPPER_ID)
-    imgImage(appVars.PARENT_WRAPPER_ID, { 'id': appVars.IMAGE_USER_ID, 'crossorigin': 'anonymous' }, rezponseObj.body.avatar_url)
+    removeNode(appVars.ID_PARENT_WRAPPER, appVars.ID_NODE_ERROR_NODE)
+    h1Heading(appVars.ID_PARENT_WRAPPER, { 'id': appVars.ID_HEADING_USERNAME }, rezponseObj.body.login)
+    ulList(rezponseObj.body, { 'id': appVars.ID_UL_USER_DEETS }, appVars.ID_PARENT_WRAPPER)
+    imgImage(appVars.ID_PARENT_WRAPPER, { 'id': appVars.ID_IMAGE_USER, 'crossorigin': 'anonymous' }, rezponseObj.body.avatar_url)
     saveToLocalStorage(rezponseObj)
 }
 
 // TODO: Smelly.
 export function removeUserDeetsNodes() {
-    removeChildNodes(appVars.PARENT_WRAPPER_ID, [appVars.HEADING_USERNAME_ID, appVars.UL_USER_DEETS_ID, appVars.IMAGE_USER_ID])
+    removeChildNodes(appVars.ID_PARENT_WRAPPER, [appVars.ID_HEADING_USERNAME, appVars.ID_UL_USER_DEETS, appVars.ID_IMAGE_USER])
 }
 
 export function toggleControlVisibility() {
-    toggleDisplayControls([appVars.DIV_INPUT_USERNAME_CONTROLS_ID, appVars.DIV_RESET_USERNAME_CONTROLS_ID])
+    toggleDisplayControls([appVars.ID_DIV_INPUT_USERNAME_CONTROLS, appVars.ID_DIV_RESET_USERNAME_CONTROLS])
 }
 
 function checkLocalStorage(uname) {
@@ -143,25 +143,36 @@ function checkLocalStorage(uname) {
         if (nDate - tsDate <= appVars.LOCAL_STORAGE_CACHE_TIME) {
             val.value.local_storage_time = tsDate;
             val.value.avatar_url = window.localStorage.getItem(`${val.value.login}_imageData`)
-            return new Rezponse(appVars.LOCAL_STORAGE_OBJECT_FOUND_STATUS, val.value)
+            return new Rezponse(appVars.STATUS_LOCAL_STORAGE_OBJECT_FOUND, val.value)
         } else {
-            return new Rezponse(appVars.LOCAL_STORAGE_OBJECT_EXPIRED_STATUS, null)
+            return new Rezponse(appVars.STATUS_LOCAL_STORAGE_OBJECT_EXPIRED, null)
         }
     } else {
-        return new Rezponse(appVars.LOCAL_STORAGE_OBJECT_NOT_FOUND_STATUS, null)
+        return new Rezponse(appVars.STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND, null)
     }
 }
 
-// TODO: Only cache if current time exceeds cachetime + cacheexpiry
+function shouldCache(uname){
+    var val = JSON.parse(window.localStorage.getItem(uname));
+    if (val != null) {
+        var tsDate = new Date(val.timestamp);
+        var nDate = new Date();
+        tsDate.setMinutes(tsDate.getMinutes() + 1)
+        return nDate > tsDate ? true : false
+    } 
+    return true;
+}
 
 function saveToLocalStorage(rezponseObj) {
+    if (!shouldCache(rezponseObj.body.login)) return
+ 
     var object = { value: rezponseObj.body, timestamp: new Date().getTime() }
     window.localStorage.setItem(rezponseObj.body.login, JSON.stringify(object));
     cacheImage(rezponseObj.body.login)
 }
 
 function cacheImage(uname) {
-    var userImageNode = document.getElementById(appVars.IMAGE_USER_ID);
+    var userImageNode = document.getElementById(appVars.ID_IMAGE_USER);
     userImageNode.addEventListener('load', function () {
         var imgData = getBase64Image(userImageNode);
         window.localStorage.setItem(`${uname}_imageData`, imgData)
@@ -177,11 +188,6 @@ function getBase64Image(imgNode) {
     context.drawImage(imgNode, 0, 0, imgNode.width, imgNode.height)
     var dataURL = canvas.toDataURL('image/png');
     return dataURL
-}
-
-// TODO: captureEnterPress
-function captureEnterPress() {
-    console.log('TODO')
 }
 
 // TODO: makeItPretty
