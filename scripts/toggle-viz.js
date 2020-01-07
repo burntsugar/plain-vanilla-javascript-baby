faf0aa5f7a81659856b6f43f63bca5e909dc76cd
burntsugar <rrakky89@gmail.com>
2020-01-07 14:51:04 +1100

    JSdoc

--- scripts/toggle-viz.js
+++ scripts/toggle-viz.js
@@ -1,24 +1,38 @@
-const toggleViz = (function () {
+/**
+ * @author Rachael Colley <rcolley@rcolley>
+ * @fileoverview Provides methods for toggling visibility of Document.documentElement.
+ * Toggle is achieved via Document.documentElement.classList.toggle('hidden-class-name').
+ * Relies upon the implementation of .hidden { display: none; } in app.css.
+ */
 
-    function toggleDisplayControls(controlz) {
-        controlz.forEach(element => {
-            document.getElementById(element).classList.toggle('hidden');
-        });
-    }
+/** RMP */
+const toggleViz = (function() {
+  /**
+   * Toggles .hidden class for a given array of Document.documentElement id's.
+   * @public
+   * @param {string[]} controlz array of Document.documentElement id's of which to toggle .hidden class.
+   * @return {undefined}
+   */
+  function toggleDisplayControls(controlz) {
+    controlz.forEach(element => {
+      toggleDisplay(element);
+    });
+  }
 
-    function toggleDisplay(elementID){
-        document.getElementById(elementID).classList.toggle('hidden');
-    }
+  /**
+   * Toggles .hidden class for a given Document.documentElement.
+   * @public
+   * @param {string} elementID Document.documentElement id of which to toggle .hidden class.
+   * @return {undefined}
+   */
+  function toggleDisplay(elementID) {
+    document.getElementById(elementID).classList.toggle('hidden');
+  }
 
-    return {
-        toggleDisplayControls: toggleDisplayControls,
-        toggleDisplay: toggleDisplay
-    };
-
-}());
+  return {
+    toggleDisplayControls: toggleDisplayControls,
+    toggleDisplay: toggleDisplay
+  };
+})();
 
 export { toggleViz };
-
-
-
-
