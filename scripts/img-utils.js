ed3f644d6ddfbbbb92afb30523816a03264934c7
burntsugar <rrakky89@gmail.com>
2020-01-09 14:57:40 +1100

    Clean-up naming

--- scripts/img-utils.js
+++ scripts/img-utils.js
@@ -10,25 +10,25 @@ const imgUtils = (function() {
   /**
    *
    * @public
-   * @param {Document.Element} imageNode the document element containing the image data to be processed.
+   * @param {documentElement} imageDocumentElement the image document element containing the image data to be processed.
    * @return {DOMString} containing base64 dataUri of the image data.
    */
-  function imageToDataURL(imageNode) {
-    return getBase64Image(imageNode);
+  function imageToDataURL(imageDocumentElement) {
+    return getBase64Image(imageDocumentElement);
   }
 
   /**
    * Constructs a Document Canvas element, upon which is drawn the provided Image Element and then encodes the result to a base64 data uri. The image's dimensions are preserved.
    * @private
-   * @param {Document.Element} imgNode the document element containing the image data to be processed.
+   * @param {Document.Element} imageDocumentElement the document element containing the image data to be processed.
    * @return {DOMString} containing base64 dataUri of the image data.
    */
-  function getBase64Image(imgNode) {
+  function getBase64Image(imageDocumentElement) {
     const canvas = document.createElement('canvas');
-    canvas.width = imgNode.width;
-    canvas.height = imgNode.height;
+    canvas.width = imageDocumentElement.width;
+    canvas.height = imageDocumentElement.height;
     const context = canvas.getContext('2d');
-    context.drawImage(imgNode, 0, 0, imgNode.width, imgNode.height);
+    context.drawImage(imageDocumentElement, 0, 0, imageDocumentElement.width, imageDocumentElement.height);
     const dataURL = canvas.toDataURL('image/png');
     return dataURL;
   }
