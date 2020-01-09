44f3a7881ca37b50801d2f86e49d66f4d4021355
burntsugar <rrakky89@gmail.com>
2020-01-10 09:50:48 +1100

    Implement RMP for app-control

--- scripts/app-control.js
+++ scripts/app-control.js
@@ -12,193 +12,202 @@ import { localStorageUtils } from './local-storage/local-storage-utils.js';
 import { commonProps } from './common-props.js';
 import { appUiHelper } from './ui-helper/app-ui-helper.js';
 
-/**
- * @public
- * Retrieves the Github user profile for the given username.
- * The username is initial cleansed and then if the profile does not exist in the cache, a network request is initiated.
- * @param {string} username Github username of the profile to retrieve.
- * @return {undefined}
- */
-export function getGithubProfile(username) {
-  let uname = prepareUsername(username);
-  if (!uname) {
-    respond(
-      new userProfile.Data(commonProps.appProps.STATUS_USERNAME_IS_EMPTY, null)
-    );
-  } else if (
-    checkLocalStorage(uname).status ==
-    commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND
-  ) {
-    respond(
-      new userProfile.Data(
-        commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND,
-        checkLocalStorage(uname).body
-      )
-    );
-  } else {
-    respond(
-      new userProfile.Data(commonProps.appProps.STATUS_START_NEW_REQUEST, {
-        login: uname
-      })
+const appControl = (function() {
+  /**
+   * @public
+   * Retrieves the Github user profile for the given username.
+   * The username is initial cleansed and then if the profile does not exist in the cache, a network request is initiated.
+   * @param {string} username Github username of the profile to retrieve.
+   * @return {undefined}
+   */
+  function getGithubProfile(username) {
+    let uname = prepareUsername(username);
+    if (!uname) {
+      respond(
+        new userProfile.Data(
+          commonProps.appProps.STATUS_USERNAME_IS_EMPTY,
+          null
+        )
+      );
+    } else if (
+      checkLocalStorage(uname).status ==
+      commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND
+    ) {
+      respond(
+        new userProfile.Data(
+          commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND,
+          checkLocalStorage(uname).body
+        )
+      );
+    } else {
+      respond(
+        new userProfile.Data(commonProps.appProps.STATUS_START_NEW_REQUEST, {
+          login: uname
+        })
+      );
+    }
+  }
+
+  /**
+   * The given username has all illegal characters removed. The username is returned, or false if it is empty,
+   * @param {string} username desc.
+   * @return {boolean} Cleansed username or else false if the string length is 0.
+   */
+  function prepareUsername(username) {
+    const uname = stringUtils.removeIllegalCharacters(username);
+    return stringUtils.stringIsEmpty(uname) ? false : uname;
+  }
+
+  /**
+   *
+   * @param {object} userProfile desc
+   */
+  function respond(userProfile) {
+    switch (userProfile.status) {
+      case commonProps.appProps.STATUS_START_NEW_REQUEST:
+        startNetworkRequest(userProfile.body.login);
+        break;
+      case commonProps.httpStatusCodes.HTTP_STATUS_SUCCESS:
+        displayProfile(userProfile);
+        break;
+      case commonProps.httpStatusCodes.HTTP_STATUS_NOT_FOUND:
+        displayError(commonProps.appProps.MESSAGE_USER_NOT_FOUND);
+        break;
+      case commonProps.domExceptionIds.NETWORK_EXC_ID:
+        displayError(commonProps.domExceptionMessages.MESSAGE_ERROR_NO_NETWORK);
+        break;
+      case commonProps.domExceptionIds.TIMEOUT_EXC_ID:
+        displayError(commonProps.domExceptionMessages.MESSAGE_ERROR_TIME_OUT);
+        break;
+      case commonProps.domExceptionIds.CANT_COMPLETE_EXC_ID:
+        displayError(
+          commonProps.domExceptionMessages.MESSAGE_OPERATION_CANT_COMPLETE
+        );
+        break;
+      case commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND:
+        displayProfile(userProfile);
+        break;
+      case commonProps.appProps.STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND:
+        displayProfile(userProfile);
+        break;
+      case commonProps.appProps.STATUS_USERNAME_IS_EMPTY:
+        displayError(commonProps.appProps.MESSAGE_USERNAME_CANNOT_BE_EMPTY);
+        break;
+      default:
+        displayError(
+          commonProps.domExceptionMessages.MESSAGE_OPERATION_CANT_COMPLETE
+        );
+        break;
+    }
+  }
+
+  /**
+   *
+   * @param {string} uname desc
+   */
+  async function startNetworkRequest(uname) {
+    const queryURL = commonProps.appProps.URL_GITHUB_USER_API + uname;
+    const result = await requestAPI(queryURL);
+    processNetworkResult(result, uname);
+  }
+
+  /**
+   *
+   * @param {object} userProfile desc
+   * @param {string} uname desc
+   */
+  function processNetworkResult(userProfile, uname) {
+    respond(userProfile);
+  }
+
+  /**
+   *
+   * @param {string} errorMessage desc
+   */
+  function displayError(errorMessage) {
+    appUiHelper.prepareErrorNode(
+      commonProps.elementIds.ID_PARENT_WRAPPER,
+      commonProps.elementIds.ID_NODE_ERROR_NODE,
+      errorMessage
     );
   }
-}
 
-/**
- * The given username has all illegal characters removed. The username is returned, or false if it is empty,
- * @param {string} username desc.
- * @return {boolean} Cleansed username or else false if the string length is 0.
- */
-function prepareUsername(username) {
-  const uname = stringUtils.removeIllegalCharacters(username);
-  return stringUtils.stringIsEmpty(uname) ? false : uname;
-}
-
-/**
- *
- * @param {object} userProfile desc
- */
-function respond(userProfile) {
-  switch (userProfile.status) {
-    case commonProps.appProps.STATUS_START_NEW_REQUEST:
-      startNetworkRequest(userProfile.body.login);
-      break;
-    case commonProps.httpStatusCodes.HTTP_STATUS_SUCCESS:
-      displayProfile(userProfile);
-      break;
-    case commonProps.httpStatusCodes.HTTP_STATUS_NOT_FOUND:
-      displayError(commonProps.appProps.MESSAGE_USER_NOT_FOUND);
-      break;
-    case commonProps.domExceptionIds.NETWORK_EXC_ID:
-      displayError(
-        commonProps.domExceptionMessages.MESSAGE_ERROR_NO_NETWORK
-      );
-      break;
-    case commonProps.domExceptionIds.TIMEOUT_EXC_ID:
-      displayError(commonProps.domExceptionMessages.MESSAGE_ERROR_TIME_OUT);
-      break;
-    case commonProps.domExceptionIds.CANT_COMPLETE_EXC_ID:
-      displayError(
-        commonProps.domExceptionMessages.MESSAGE_OPERATION_CANT_COMPLETE
-      );
-      break;
-    case commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND:
-      displayProfile(userProfile);
-      break;
-    case commonProps.appProps.STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND:
-      displayProfile(userProfile);
-      break;
-    case commonProps.appProps.STATUS_USERNAME_IS_EMPTY:
-      displayError(commonProps.appProps.MESSAGE_USERNAME_CANNOT_BE_EMPTY);
-      break;
-    default:
-      displayError(
-        commonProps.domExceptionMessages.MESSAGE_OPERATION_CANT_COMPLETE
-      );
-      break;
-  }
-}
-
-/**
- *
- * @param {string} uname desc
- */
-async function startNetworkRequest(uname) {
-  const queryURL = commonProps.appProps.URL_GITHUB_USER_API + uname;
-  const result = await requestAPI(queryURL);
-  processNetworkResult(result, uname);
-}
-
-/**
- *
- * @param {object} userProfile desc
- * @param {string} uname desc
- */
-function processNetworkResult(userProfile, uname) {
-  respond(userProfile);
-}
-
-/**
- *
- * @param {string} errorMessage desc
- */
-function displayError(errorMessage) {
-  appUiHelper.prepareErrorNode(
-    commonProps.elementIds.ID_PARENT_WRAPPER,
-    commonProps.elementIds.ID_NODE_ERROR_NODE,
-    errorMessage
-  );
-}
-
-/**
- *
- * @param {object} userProfile
- */
-function displayProfile(userProfile) {
-  toggleControlsVisibility([
+  /**
+   *
+   * @param {object} userProfile
+   */
+  function displayProfile(userProfile) {
+    toggleControlsVisibility([
       commonProps.elementIds.ID_DIV_INPUT_USERNAME_CONTROLS,
       commonProps.elementIds.ID_DIV_RESET_USERNAME_CONTROLS
     ]);
-  appUiHelper.prepareSuccessNodes(userProfile);
-  saveToLocalStorage(userProfile);
-}
-
-export function removeProfile() {
-  appUiHelper.removeUserDeetsNodes(commonProps.elementIds.ID_PARENT_WRAPPER, [
-    commonProps.elementIds.ID_HEADING_USERNAME,
-    commonProps.elementIds.ID_UL_USER_DEETS,
-    commonProps.elementIds.ID_IMAGE_USER
-  ]);
-}
-
-/**
- *
- */
-export function toggleControlsVisibility(controls) {
-  appUiHelper.toggleControlsVisibility(controls);
-}
-
-/**
- *
- * @param {string} uname
- * @return {object}
- */
-function checkLocalStorage(uname) {
-  const val = localStorageUtils.retrieveFromLocalStorage(uname);
-  if (val != null) {
-    val.avatar_url = localStorageUtils.retrieveImageFromLocalStorage(
-      `${val.login}_imageData`
-    );
-    return new userProfile.Data(
-      commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND,
-      val
-    );
-  } else {
-    return new userProfile.Data(
-      commonProps.appProps.STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND,
-      null
-    );
+    appUiHelper.prepareSuccessNodes(userProfile);
+    saveToLocalStorage(userProfile);
   }
-}
 
-/**
- *
- * @param {object} userProfile
- */
-function saveToLocalStorage(userProfile) {
-  localStorageUtils.persistToLocalStorage(
-    userProfile.body.login,
-    userProfile.body
-  );
-  const userImageNode = document.getElementById(
-    commonProps.elementIds.ID_IMAGE_USER
-  );
-  userImageNode.addEventListener('load', function() {
-    localStorageUtils.persistImageToLocalStorage(
-      `${userProfile.body.login}_imageData`,
-      imgUtils.imageToDataURL(userImageNode)
+  function removeProfile() {
+    appUiHelper.removeUserDeetsNodes(commonProps.elementIds.ID_PARENT_WRAPPER, [
+      commonProps.elementIds.ID_HEADING_USERNAME,
+      commonProps.elementIds.ID_UL_USER_DEETS,
+      commonProps.elementIds.ID_IMAGE_USER
+    ]);
+  }
+
+  /**
+   *
+   */
+  function toggleControlsVisibility(controls) {
+    appUiHelper.toggleControlsVisibility(controls);
+  }
+
+  /**
+   *
+   * @param {string} uname
+   * @return {object}
+   */
+  function checkLocalStorage(uname) {
+    const val = localStorageUtils.retrieveFromLocalStorage(uname);
+    if (val != null) {
+      val.avatar_url = localStorageUtils.retrieveImageFromLocalStorage(
+        `${val.login}_imageData`
+      );
+      return new userProfile.Data(
+        commonProps.localStorageStatus.STATUS_LOCAL_STORAGE_OBJECT_FOUND,
+        val
+      );
+    } else {
+      return new userProfile.Data(
+        commonProps.appProps.STATUS_LOCAL_STORAGE_OBJECT_NOT_FOUND,
+        null
+      );
+    }
+  }
+
+  /**
+   *
+   * @param {object} userProfile
+   */
+  function saveToLocalStorage(userProfile) {
+    localStorageUtils.persistToLocalStorage(
+      userProfile.body.login,
+      userProfile.body
     );
-  });
-}
+    const userImageNode = document.getElementById(
+      commonProps.elementIds.ID_IMAGE_USER
+    );
+    userImageNode.addEventListener('load', function() {
+      localStorageUtils.persistImageToLocalStorage(
+        `${userProfile.body.login}_imageData`,
+        imgUtils.imageToDataURL(userImageNode)
+      );
+    });
+  }
 
-/** TODO: Would you like to see a cached result? */
+  return {
+    getGithubProfile: getGithubProfile,
+    removeProfile: removeProfile,
+    toggleControlsVisibility: toggleControlsVisibility
+  };
+})();
+
+export { appControl };
