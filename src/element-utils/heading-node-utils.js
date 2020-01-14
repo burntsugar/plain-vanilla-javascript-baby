572bf3386e4b19786f4a2d2297349fef81f7c587
burntsugar <rrakky89@gmail.com>
2020-01-14 15:57:02 +1100

    implement arrow function syntax

--- src/element-utils/heading-node-utils.js
+++ src/element-utils/heading-node-utils.js
@@ -4,18 +4,18 @@
  * @fileoverview Provides methods for adding h1 element nodes to the document.
  */
 
- /**
-  * RMP
-  */
-const headingNodeUtils = (function() {
-    /**
-     * @public
-     * Prepares a h1 element node which is then attached to the given parent node.
-     * @param {string} parentNodeID the id of the parent node
-     * @param {string[]} nodeAttributes attributes belonging to the new h1 node
-     * @param {string} text text contents of the h1 node
-     */
-  function h1Heading(parentNodeID, nodeAttributes, text) {
+/**
+ * RMP
+ */
+const headingNodeUtils = (() => {
+  /**
+   * @public
+   * Prepares a h1 element node which is then attached to the given parent node.
+   * @param {string} parentNodeID the id of the parent node
+   * @param {string[]} nodeAttributes attributes belonging to the new h1 node
+   * @param {string} text text contents of the h1 node
+   */
+  const h1Heading = (parentNodeID, nodeAttributes, text) => {
     var h1 = document.createElement('h1');
 
     Object.keys(nodeAttributes).forEach(element => {
@@ -25,7 +25,7 @@ const headingNodeUtils = (function() {
     var textNode = document.createTextNode(text);
     h1.appendChild(textNode);
     document.getElementById(parentNodeID).appendChild(h1);
-  }
+  };
 
   return {
     h1Heading: h1Heading
