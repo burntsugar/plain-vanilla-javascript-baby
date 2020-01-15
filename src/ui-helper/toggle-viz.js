a9a63a568d94b967c4d0d73045c8d7be54f4b292
burntsugar <rrakky89@gmail.com>
2020-01-15 13:43:30 +1100

    linting...

--- src/ui-helper/toggle-viz.js
+++ src/ui-helper/toggle-viz.js
@@ -2,8 +2,10 @@
 
 /**
  * @author Rachael Colley <rcolley@rcolley>
- * @fileoverview Provides methods for toggling visibility of Document.documentElement.
- * Toggle is achieved via Document.documentElement.classList.toggle('hidden-class-name').
+ * @fileoverview Provides methods for toggling visibility of
+ * Document.documentElement.
+ * Toggle is achieved via Document.documentElement.classList.toggle
+ * ('hidden-class-name').
  * Relies upon the implementation of .hidden { display: none; } in app.css.
  */
 
@@ -12,11 +14,12 @@ const toggleViz = (() => {
   /**
    * Toggles .hidden class for a given array of Document.documentElement id's.
    * @public
-   * @param {string[]} controlz array of Document.documentElement id's of which to toggle .hidden class.
+   * @param {string[]} controlz array of Document.documentElement id's of
+   * which to toggle .hidden class.
    * @return {undefined}
    */
-  const toggleDisplayControls = controlz => {
-    controlz.forEach(element => {
+  const toggleDisplayControls = (controlz) => {
+    controlz.forEach((element) => {
       toggleDisplay(element);
     });
   };
@@ -24,16 +27,17 @@ const toggleViz = (() => {
   /**
    * Toggles .hidden class for a given Document.documentElement.
    * @public
-   * @param {string} elementID Document.documentElement id of which to toggle .hidden class.
+   * @param {string} elementID Document.documentElement id of which to
+   * toggle .hidden class.
    * @return {undefined}
    */
-  const toggleDisplay = elementID =>
+  const toggleDisplay = (elementID) =>
     void document.getElementById(elementID).classList.toggle('hidden');
 
   return {
     toggleDisplayControls: toggleDisplayControls,
-    toggleDisplay: toggleDisplay
+    toggleDisplay: toggleDisplay,
   };
 })();
 
-export { toggleViz };
+export {toggleViz};
