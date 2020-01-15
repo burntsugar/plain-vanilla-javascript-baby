d7c00696e6eebab4cb0a450108f57dc949dc85cf
burntsugar <rrakky89@gmail.com>
2020-01-15 12:21:03 +1100

    refactor Data to ES6 class from constructor function

--- src/user/user-profile.js
+++ src/user/user-profile.js
@@ -2,24 +2,46 @@
 
 /**
  * @author Rachael Colley <rcolley@rcolley>
- * @fileoverview Produces a Data object, containing given user data.
+ * @fileoverview Produces a Data instance, containing given user data.
  */
 
 /** RMP */
 const userProfile = ( () => {
   /**
-   * Constructor function.
-   * Returns a Data object which contains given data for a user.
-   * @param {number} status the status code representing the state of
-   * this user's data after a network request or other retrieval
-   * operation, or validation activity
-   * @param {string} body data for this user. In the case of a
-   * successful retrieval, this will be a JSON string. Or else
-   * error information
-   */
-  function Data(status, body) {
-    this.status = status;
-    this.body = body;
+     * Returns a Data instance which contains given data for a user.
+     * @param {number} status the status code representing the state of
+     * this user's data after a network request or other retrieval
+     * operation, or validation activity
+     * @param {string} body data for this user. In the case of a
+     * successful retrieval, this will be a JSON string. Or else
+     * error information
+     */
+  class Data {
+    /**
+     *
+     * @param {string} status
+     * @param {object} body
+     */
+    constructor(status, body) {
+      this._status = status;
+      this._body = body;
+    }
+
+    /**
+     * get status.
+     * @return {string} status
+     */
+    get status() {
+      return this._status;
+    }
+
+    /**
+     * get body
+     * @return {object} body
+     */
+    get body() {
+      return this._body;
+    }
   }
   return {
     Data: Data,
