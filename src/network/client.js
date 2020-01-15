49cac14072b4ed25e2b5437f93b78861b205dbfb
burntsugar <rrakky89@gmail.com>
2020-01-15 13:48:16 +1100

    linting...

--- src/network/client.js
+++ src/network/client.js
@@ -4,13 +4,13 @@
  * @fileoverview does stuff.
  */
 
-import { commonProps } from '../common-props.js';
+import {commonProps} from '../common-props.js';
 
 /**
- *
+ * @async
  * @param {string} url api url.
  */
- const fetchJsonResource = async (url) => {
+const fetchJsonResource = async (url) => {
   return new Promise(function(resolve, reject) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -18,31 +18,31 @@ import { commonProps } from '../common-props.js';
     xhr.responseType = 'json';
     // callback
     xhr.onload = function() {
-      resolve({ status: this.status, body: this.response });
+      resolve({status: this.status, body: this.response});
     };
-    xhr.ontimeout = e => {
+    xhr.ontimeout = (e) => {
       console.error('Timeout!!');
       reject(
         new DOMException(
           commonProps.domExceptionNames.ERROR_NAME_TIME_OUT_ERROR,
-          commonProps.domExceptionIds.TIMEOUT_EXC_ID
-        )
+          commonProps.domExceptionIds.TIMEOUT_EXC_ID,
+        ),
       );
     };
     xhr.onloadend = () => {
       console.log(`xhr.status: ${xhr.status}`);
     };
-    xhr.onerror = e => {
+    xhr.onerror = (e) => {
       console.error('Request failed: Network error');
       reject(
         new DOMException(
           commonProps.domExceptionNames.ERROR_NAME_NO_NETWORK_ERROR,
-          commonProps.domExceptionIds.NETWORK_EXC_ID
-        )
+          commonProps.domExceptionIds.NETWORK_EXC_ID,
+        ),
       );
     };
     xhr.send();
   });
-}
+};
 
 export {fetchJsonResource};
