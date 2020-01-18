c7a755ff837dd694246b612935d9a2412d5bf87a
burntsugar <rrakky89@gmail.com>
2020-01-18 12:38:30 +1100

    Added property and setter for fetch client object

--- src/network/network-control.js
+++ src/network/network-control.js
@@ -14,6 +14,20 @@ import {commonProps} from '../common-props.js';
  * @description Provides access to network.
  */
 const networkControl = (() => {
+
+  /**
+   * Object in use for fetch.
+   */
+  let fetchClient = fetchJsonResource;
+
+  /**
+   * Use to change the default fetch client object.
+   * Mocking is a possible use case.
+   * client.js is used by default.
+   * @param {*} client 
+   */
+  const setFetchClient = (client) => (fetchClient = client);
+  
   /**
    * @type {Response} instance of Response
    */
@@ -27,7 +41,7 @@ const networkControl = (() => {
    * @param {string} url to request
    */
   const startRequest = async (url) => {
-    await fetchJsonResource(url)
+    await fetchClient(url)
       .then((data) => {
         responseData = new Response(data.status, data.body);
       })
@@ -89,7 +103,109 @@ const networkControl = (() => {
 
   return {
     requestAPI: requestAPI,
+    setFetchClient: setFetchClient,
   };
 })();
 
 export {networkControl};
+
+
+//////////////
+// 'use strict';
+// /**
+//  * @file
+//  * @author Rachael Colley <rcolley@rcolley>
+//  * @fileoverview Provides access to network.
+//  */
+
+// import {fetchJsonResource} from './client.js';
+// import {checkNetwork} from './check-network.js';
+// import {commonProps} from '../common-props.js';
+
+// /**
+//  * @module networkControl
+//  * @description Provides access to network.
+//  */
+// const networkControl = (() => {
+
+//   let client = fetchJsonResource;
+
+//   /**
+//    * @type {Response} instance of Response
+//    */
+//   let responseData;
+
+//   /**
+//    * @async
+//    * Starts a network request
+//    * If the request is successful, data is made available to the block.
+//    * If the request throws an error, the error is made available to the block.
+//    * @param {string} url to request
+//    */
+//   const startRequest = async (url) => {
+//     await fetchJsonResource(url)
+//       .then((data) => {
+//         responseData = new Response(data.status, data.body);
+//       })
+//       .catch((error) => {
+//         responseData = new Response(error.message, error.name);
+//       });
+//   };
+
+//   /**
+//    * @async
+//    * Starts a network request.
+//    * Initially checks for a network connection which if not available then
+//    * propogates an error message.
+//    * @public
+//    * @param {string} apiUrl to request
+//    * @return {object} instance of Response
+//    */
+//   const requestAPI = async (apiUrl) => {
+//     if (checkNetwork.hasNoConnection()) {
+//       responseData = new Response(commonProps.domExceptionIds.NETWORK_EXC_ID,
+//         commonProps.domExceptionMessages.MESSAGE_ERROR_NO_NETWORK);
+//       return responseData;
+//     }
+//     await startRequest(apiUrl);
+//     return responseData;
+//   };
+
+
+//   /**
+//    * @class Response
+//    * @description Defines an object which contains the response
+//    * of the network operation.
+//    */
+//   class Response {
+//     /**
+//      * @constructor
+//      * @param {*} status
+//      * @param {*} body
+//      */
+//     constructor(status, body) {
+//       this._status = status;
+//       this._body = body;
+//     }
+
+//     /**
+//      *
+//      */
+//     get status() {
+//       return this._status;
+//     }
+
+//     /**
+//      *
+//      */
+//     get body() {
+//       return this._body;
+//     }
+//   }
+
+//   return {
+//     requestAPI: requestAPI,
+//   };
+// })();
+
+// export {networkControl};
